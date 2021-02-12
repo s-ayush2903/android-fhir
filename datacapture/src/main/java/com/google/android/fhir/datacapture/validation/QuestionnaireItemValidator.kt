b877afd56dd845c02f0b9258706efd689d397651
Kunjan <kunjanp@argusoft.com>
2021-02-13 01:10:21 +0530

    Merge conflict resolution

--- datacapture/src/main/java/com/google/android/fhir/datacapture/validation/QuestionnaireItemValidator.kt
+++ datacapture/src/main/java/com/google/android/fhir/datacapture/validation/QuestionnaireItemValidator.kt
@@ -1,21 +1,17 @@
 package com.google.android.fhir.datacapture.validation
 
+import com.google.fhir.r4.core.Extension
 import com.google.fhir.r4.core.Questionnaire
 import com.google.fhir.r4.core.QuestionnaireResponse
 
 interface QuestionnaireItemValidator {
-
-    /**
-     * Validates [questionnaireResponseItemBuilder] contains valid answer(s) to [questionnaireItem].
-     */
-    fun validate(questionnaireItem: Questionnaire.Item, questionnaireResponseItemBuilder: QuestionnaireResponse.Item.Builder): ValidationResult
 
     companion object {
         const val EXTENSION_MAX_VALUE = "http://hl7.org/fhir/StructureDefinition/maxValue"
         const val EXTENSION_MIN_VALUE = "http://hl7.org/fhir/StructureDefinition/minValue"
 
         /**
-         * Validates the input data as per the ViewHolder and returns the result and its resultant error messages (if any)
+         * Validates [questionnaireResponseItemBuilder] contains valid answer(s) to [questionnaireItem].
          */
         fun validate(questionnaireItem: Questionnaire.Item, questionnaireResponseItemBuilder: QuestionnaireResponse.Item.Builder): ValidationResult {
             if (questionnaireResponseItemBuilder.answerBuilderList.isEmpty()) {
