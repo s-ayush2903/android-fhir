f5a8a918d490634af0e233ed1cab8f97a6b38e3e
s-ayush2903 <ayush_1901cb14@iitp.ac.in>
2021-03-29 01:12:11 +0530

    do not cancel wf on subsequent pushes -_-

--- datacapture/src/androidTest/java/com/google/android/fhir/datacapture/views/QuestionnaireItemCheckBoxViewHolderFactoryInstrumentedTest.kt
+++ datacapture/src/androidTest/java/com/google/android/fhir/datacapture/views/QuestionnaireItemCheckBoxViewHolderFactoryInstrumentedTest.kt
@@ -194,17 +194,17 @@ class QuestionnaireItemCheckBoxViewHolderFactoryInstrumentedTest {
       assertThat(this.isChecked).isTrue()
     }
 
-//    val questionnaireItemViewItem =
-//      QuestionnaireItemViewItem(
-//        Questionnaire.QuestionnaireItemComponent().apply { text = "Question?" },
-//        QuestionnaireResponse.QuestionnaireResponseItemComponent()
-//      ) {}
-//    viewHolder.bind(questionnaireItemViewItem)
-//    //    viewHolder.itemView.findViewById<CheckBox>(R.id.check_box).isChecked
-//
-//    val checkBox = viewHolder.itemView.findViewById<CheckBox>(R.id.check_box)
-//    checkBox.performClick()
-//    assertThat(checkBox.isChecked).isTrue()
+    //    val questionnaireItemViewItem =
+    //      QuestionnaireItemViewItem(
+    //        Questionnaire.QuestionnaireItemComponent().apply { text = "Question?" },
+    //        QuestionnaireResponse.QuestionnaireResponseItemComponent()
+    //      ) {}
+    //    viewHolder.bind(questionnaireItemViewItem)
+    //    //    viewHolder.itemView.findViewById<CheckBox>(R.id.check_box).isChecked
+    //
+    //    val checkBox = viewHolder.itemView.findViewById<CheckBox>(R.id.check_box)
+    //    checkBox.performClick()
+    //    assertThat(checkBox.isChecked).isTrue()
     //    checkBox.performClick()
     //    assertThat(checkBox.isChecked).isFalse()
   }
